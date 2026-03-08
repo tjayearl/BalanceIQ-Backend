c09818d516faea75580c1ffafbb52406620d7f09
tjayearl <iamtjayearl@gmail.com>
2026-03-08 11:39:38 +0300

    modified:   db.py

--- db.py
+++ db.py
@@ -1,9 +1,18 @@
 import psycopg2
+import os
 
 def get_db():
-    return psycopg2.connect(
-        dbname="balanceiq",
-        user="balanceuser",
-        password="balancepass",
-        host="localhost"
-    )
+    database_url = os.environ.get("DATABASE_URL")
+    
+    if database_url:
+        # Fix: Render uses postgres:// but psycopg2 needs postgresql://
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace("postgres://", "postgresql://", 1)
+        return psycopg2.connect(database_url, sslmode="require")
+    else:
+        return psycopg2.connect(
+            dbname="balanceiq",
+            user="balanceuser",
+            password="balancepass",
+            host="localhost"
+        )
